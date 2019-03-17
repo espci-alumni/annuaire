50c7acb094f791aa7a1b10c99f2c69bdca929131
espci.org <postmaster@espci.org>
2019-03-17 15:26:34 +0100

    n'autoriser l'ancien annuaire qu'aux admins

--- tribes/class/agent.php
+++ tribes/class/agent.php
@@ -4,10 +4,10 @@ class agent extends self
 {
     function control()
     {
-        if (!SESSION::get('acces'))
+        if ('admin' !== SESSION::get('acces'))
         {
             SESSION::flash('referer', Patchwork::__URI__());
-            Patchwork::redirect($CONFIG['tribes.baseUrl'] . 'login');
+            Patchwork::redirect($CONFIG['tribes.baseUrl']);
         }
     }
 }
